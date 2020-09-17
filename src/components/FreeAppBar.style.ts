e21226bfbde8f103b6c799e8b6ef23da5a9ae0bc
Baptiste BORDET <bbordet@corp.free.fr>
2020-09-17 12:11:43 +0200

    init test

---
+++ src/components/FreeAppBar.style.ts
@@ -0,0 +1,9 @@
+import { makeStyles } from "@material-ui/core/styles";
+
+const useStyles = makeStyles({
+  logo: {
+    width: '4rem',
+  },
+})
+
+export default useStyles
---
+++ src/components/FreeAppBar.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import { AppBar, IconButton, Toolbar, Typography } from '@material-ui/core';
+import useStyles from "./FreeAppBar.style"
+
+const FreeAppBar = () => {
+  const classes = useStyles();
+
+  return (
+    <AppBar position="static">
+      <Toolbar>
+        <IconButton edge="start" color="inherit" aria-label="menu">
+          <img src="/assets/logo-free.png" className={classes.logo} alt="logo" />
+        </IconButton>
+        <Typography variant="h4">Movie Finder</Typography>
+      </Toolbar>
+    </AppBar>
+  )
+}
+
+export default FreeAppBar
